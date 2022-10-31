99e6fe95532cac642d5f255e83192bf6349fd867
spidyansh <aggarwalanshul1997@gmail.com>
2022-10-31 21:19:00 +0530

    first commit

---
+++ logic.js
@@ -0,0 +1,28 @@
+
+let btn = document.getElementById("btn")
+
+btn.addEventListener("click", () => {
+    let A = getRandomNumberA()
+    let B = getRandomNumberB()
+    let C = getRandomNumberC()
+    let D = getRandomNumberD()
+    m = "rgba(" + A + "," + B + "," + C + "," + D + ")"
+    document.body.style.backgroundColor = m
+})
+
+function getRandomNumberA() {
+    return Math.floor(Math.random() * 255)
+
+}
+function getRandomNumberB() {
+    return Math.floor(Math.random() * 255)
+
+}
+function getRandomNumberC() {
+    return Math.floor(Math.random() * 255)
+}
+function getRandomNumberD() {
+    return (Math.random()* 1)
+}
+console.log(getRandomNumberD())
+
